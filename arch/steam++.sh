b15f3b135cce30de00cc66a7aee6ab84ddb39b10
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-05-30 12:07:42 +0800

    更新steam++.sh(nyarch)

--- arch/steam++.sh
+++ arch/steam++.sh
@@ -1,8 +1,8 @@
-CER="$HOME/.local/share/Steam++/Plugins/Accelerator/SteamTools.Certificate.cer"
-sudo cp "$CER" /etc/ca-certificates/trust-source/anchors/
+cat << EOF
+目前在NyArch上测试过，执行完后，重启。
+重启后，再次打开，仍汇报错，再次打开，证书异常解决
+EOF
+read test
+
+sudo trust anchor --store SteamTools.Certificate.cer
 sudo update-ca-trust extract
-
-echo "alias watt-toolkit='watt-toolkit --no-browser-certificate'" >> ~/.bashrc
-source ~/.bashrc
-
-sudo chmod a+w /etc/hosts
